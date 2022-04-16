267b83e943a09d81028174c314ccecaf5730f4ad
ShlykovN <nshfind@gmail.com>
2022-04-16 16:15:48 +0300

    Шлыков Н.С. Задание - Повторение действий

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,13 +1,39 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
+
+    //let data;
+    data = JSON.parse(json);
+   // let products;
+    products = data.products;
+    return products;
+
+}
+
+function renderProductsCards(json){
+    
+    clearProducts(); 
+    products = parseProducts(json);
+     length = products.length;
+     for (let i = 0; i < length; i += 1) {
+       addProduct(products[i]);
+        }      
+    }
+
+
+  
+
+
+
+
+
     // Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
+
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
