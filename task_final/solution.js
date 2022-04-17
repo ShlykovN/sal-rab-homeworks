41f5ba11d35e2f4c154b16131818cb202ae6156f
ShlykovN <nshfind@gmail.com>
2022-04-17 22:55:25 +0300

    1

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,32 +33,21 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum,) {
-  
-    
-    let data = {client: {name, phone}, order: {address, sum}, goods: []};
-       
- 
- 
-     let countOfGoods = goods.length;
- 
-     for (let i = 0; i < countOfGoods; i += 1) {
-         data.goods.push( {title: goods[i].title,  count: goods[i].count} ); 
-     }
- 
-     // let address = {street, house, entrance, floor, flat};
-     // data.order.address = address ;
-     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {goods: [], order: {}};
 
-    
-     data.order.sum =  sum;
- 
-     data.client = name + " " + phone;
- 
- 
-         let jsonData = JSON.stringify({data: data});
- 
- 
-     return jsonData;
- }
- 
+    let countOfGoods = goods.length;
+
+    for (let i = 0; i <= countOfGoods; i += 1) {
+        data.goods.push(goods[i].title);
+    }
+
+    data.order.address = address;
+    data.order.sum = name + phone + address + goods + sum;
+
+    data.client = 'Иван';
+
+    let jsonData = JSON.stringify(data);
+
+    return jsonData;
+}
